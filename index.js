bbc4617af4d17d85ce8f13307884b1ff831f9936
Suleiman <real1sule@gmail.com>
2025-09-19 06:19:38 +0300

    completed arrays filtering method lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution here
+// FIND MATCHING
+function findMatching(drivers, name) {
+  return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+//FUZZY MATCH
+function fuzzyMatch(drivers, letters) {
+  return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+}
+//MATCHNAME
+function matchName(drivers, name) {
+  return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+}
+
+//Example usage
+
+const drivers = ['Suleiman', 'Mumbi', 'Chris', 'Alea', 'Abdul']
+
+const driversObject = [
+  {name: "Suleiman", hometown: "Nairobi"},
+  { name: "Mumbi", hometown: "Kiambu" },
+  { name: "Chris", hometown: "Kisii" },
+  { name: "Alea", hometown: "Kisumu" },
+  { name: "Abdul", hometown: "Turkana" }
+]
+
+console.log(findMatching(drivers, 'Suleiman'));
